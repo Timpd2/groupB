69dc2da82774788fd4d5709683e109cdb3498e35
Timpd2 <timhome4@gmail.com>
2025-11-25 16:21:20 +0800

    finish/float

--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 int add(int a, int b) {
     return a + b;
 }       
-
+int add_float(float a, float b) {
+    return a + b;
+}       
 
 int main() {
     int a=5;
     int b=13;
+    int a=5.5;
+    int b=13.2;
     printf("%d + %d =%d\n",a,b,add(a,b));
+    printf("%f + %f =%f\n",a,b,add_float(a,b));
     return 0;
 }
